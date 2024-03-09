ef09733138197ac40ac7cbae502bfe17f6e4cc61
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-03-09 22:54:03 +0700

    Update title

--- src/controllers/publicController.js
+++ src/controllers/publicController.js
@@ -10,15 +10,12 @@ import { UserModel } from "../models/UserModel.js";
 const User = new UserModel();
 
 // Import VIEWS path
-import {
-  VIEWS
-} from "./../config/app-config.js";
+import { VIEWS } from "./../config/app-config.js";
 
 // Controller class
 export class PublicController {
-
-  async paginatedIndex(req, res){
-    try{
+  async paginatedIndex(req, res) {
+    try {
       let results = await Product.getNumProducts();
       let numProducts = results[0].numProducts;
       let pageSize = Product.pageSize;
@@ -27,150 +24,156 @@ export class PublicController {
       let images = await Image.get();
       let user = req.user;
       let cart = [];
-      if (typeof(user) !== "undefined") {
+      if (typeof user !== "undefined") {
         cart = await Product.getUserCart(user.id);
       }
-      
-      res.render(
-        path.resolve(VIEWS, "public", "homepage"), {
-          title: "Homepage",
-          images: images,
-          products: products,
-          totalPages: totalPages,
-          user: user,
-          cart: cart,
-          csrfToken: req.csrfToken()
-        }
-      )
-    } catch(e) {
+
+      res.render(path.resolve(VIEWS, "public", "homepage"), {
+        title: "Homepage 123",
+        images: images,
+        products: products,
+        totalPages: totalPages,
+        user: user,
+        cart: cart,
+        csrfToken: req.csrfToken(),
+      });
+    } catch (e) {
       throw e;
     }
   }
 
-  async getPageContent(req, res){
-    try{
+  async getPageContent(req, res) {
+    try {
       let products = await Product.getPage(req.params.page);
       let images = await Image.get();
       let user = req.user;
       let cart = [];
-      if (typeof(user) !== "undefined") {
+      if (typeof user !== "undefined") {
         cart = await Product.getUserCart(user.id);
       }
       res.send({
         products: products,
         images: images,
-        cart: cart
+        cart: cart,
       });
-    } catch(e) {
+    } catch (e) {
       throw e;
     }
   }
 
-  async goToProduct(req, res){
+  async goToProduct(req, res) {
     try {
       let product = await Product.getById(req.params.id);
       let images = await Image.getByProductId(req.params.id);
       let user = req.user;
       let cart = [];
-      if (typeof(user) !== "undefined") {
+      if (typeof user !== "undefined") {
         cart = await Product.getUserCart(user.id);
       }
 
-      res.render(
-        path.resolve(VIEWS, "public", "product", "product-details"), {
-          title: "Product",
-          product: product,
-          images: images,
-          user: user,
-          cart: cart,
-          csrfToken: req.csrfToken()
-        }
-      );
-    } catch(e) {
-      res.status(404).render(path.resolve(VIEWS, "404.ejs"), {title: "Error", layout: "./public/layouts/layout-user"});
+      res.render(path.resolve(VIEWS, "public", "product", "product-details"), {
+        title: "Product",
+        product: product,
+        images: images,
+        user: user,
+        cart: cart,
+        csrfToken: req.csrfToken(),
+      });
+    } catch (e) {
+      res
+        .status(404)
+        .render(path.resolve(VIEWS, "404.ejs"), {
+          title: "Error",
+          layout: "./public/layouts/layout-user",
+        });
       throw e;
     }
   }
 
-  async goToCart(req, res){
+  async goToCart(req, res) {
     let user = req.user;
     let cart = [];
     let products = [];
     let images = [];
-    if (typeof(user) !== "undefined") {
+    if (typeof user !== "undefined") {
       cart = await Product.getUserCart(user.id);
       products = await Product.get();
       images = await Image.get();
     }
 
-    res.render(
-      path.resolve(VIEWS, "public", "product", "cart.ejs"), {
-        title: "Shopping cart",
-        user: user,
-        cart: cart,
-        products: products,
-        images: images,
-        csrfToken: req.csrfToken()
-      }
-    );
+    res.render(path.resolve(VIEWS, "public", "product", "cart.ejs"), {
+      title: "Shopping cart",
+      user: user,
+      cart: cart,
+      products: products,
+      images: images,
+      csrfToken: req.csrfToken(),
+    });
   }
 
-  async addToCart(req, res){
-    try{
-      const { customer_id, product_id } = req.body
-      let resultStock = await Product.updateStock(product_id, -1)
-      let resultCart = await Product.createInCart(customer_id, product_id)
+  async addToCart(req, res) {
+    try {
+      const { customer_id, product_id } = req.body;
+      let resultStock = await Product.updateStock(product_id, -1);
+      let resultCart = await Product.createInCart(customer_id, product_id);
       res.send({
         messageStock: resultStock,
-        messageCart: resultCart
+        messageCart: resultCart,
       });
-    } catch(e) {
+    } catch (e) {
       res.send({
-        message: e.message
+        message: e.message,
       });
       throw e;
     }
   }
 
-  async removeFromCart(req, res){
-    try{
-      const { customer_id, product_id } = req.body
-      let cartItems = await Product.getCartItem(customer_id, product_id)
-      let resultStock = await Product.updateStock(product_id, cartItems[0].quantity)
-      let resultCart = await Product.deleteInCart(customer_id, product_id)
+  async removeFromCart(req, res) {
+    try {
+      const { customer_id, product_id } = req.body;
+      let cartItems = await Product.getCartItem(customer_id, product_id);
+      let resultStock = await Product.updateStock(
+        product_id,
+        cartItems[0].quantity,
+      );
+      let resultCart = await Product.deleteInCart(customer_id, product_id);
       res.send({
         messageStock: resultStock,
-        messageCart: resultCart
+        messageCart: resultCart,
       });
-    } catch(e) {
+    } catch (e) {
       res.send({
-        message: e.message
+        message: e.message,
       });
       throw e;
     }
   }
 
-  async updateCart(req, res){
-    try{
-      const { customer_id, product_id, new_quantity } = req.body
-      let cartItems = await Product.getCartItem(customer_id, product_id)
-      let difference = cartItems[0].quantity - new_quantity
+  async updateCart(req, res) {
+    try {
+      const { customer_id, product_id, new_quantity } = req.body;
+      let cartItems = await Product.getCartItem(customer_id, product_id);
+      let difference = cartItems[0].quantity - new_quantity;
       let resultStock = await Product.updateStock(product_id, difference);
-      let resultCart = await Product.updateInCart(customer_id, product_id, new_quantity)
+      let resultCart = await Product.updateInCart(
+        customer_id,
+        product_id,
+        new_quantity,
+      );
 
       res.send({
         messageStock: resultStock,
-        messageCart: resultCart
+        messageCart: resultCart,
       });
-    } catch(e) {
+    } catch (e) {
       res.send({
-        message: e.message
+        message: e.message,
       });
       throw e;
     }
   }
 
-  async goToCheckout(req, res){
+  async goToCheckout(req, res) {
     let step = req.params.step;
     let checkoutView = "checkout-" + step + ".ejs";
 
@@ -180,7 +183,7 @@ export class PublicController {
     let images = [];
     let lastOrders = [];
     let ordersItems = [];
-    if (typeof(user) !== "undefined") {
+    if (typeof user !== "undefined") {
       cart = await Product.getUserCart(user.id);
       products = await Product.get();
       images = await Image.get();
@@ -188,30 +191,23 @@ export class PublicController {
       ordersItems = await Product.getOrderItems();
     }
 
-    res.render(
-      path.resolve(VIEWS, "public", "product", checkoutView), {
-        title: "Checkout",
-        user: user,
-        cart: cart,
-        products: products,
-        images: images,
-        lastOrders: lastOrders,
-        ordersItems: ordersItems,
-        csrfToken: req.csrfToken()
-      }
-    );
+    res.render(path.resolve(VIEWS, "public", "product", checkoutView), {
+      title: "Checkout",
+      user: user,
+      cart: cart,
+      products: products,
+      images: images,
+      lastOrders: lastOrders,
+      ordersItems: ordersItems,
+      csrfToken: req.csrfToken(),
+    });
   }
 
-  async validateShipping(req, res){
-    const {
-      address,
-      zipCode,
-      country,
-      phoneNumber
-    } = req.body
+  async validateShipping(req, res) {
+    const { address, zipCode, country, phoneNumber } = req.body;
 
     let cart = [];
-    if (typeof(req.user) !== "undefined") {
+    if (typeof req.user !== "undefined") {
       cart = await Product.getUserCart(req.user.id);
     }
 
@@ -219,31 +215,35 @@ export class PublicController {
     /* Validation (we could place here whatever 
       we wanted to validate for each field) */
 
-    if(!validated){
-      let error = new Error("Invalid input fields")
-      res.render(
-        path.resolve(VIEWS, "public", "product", "checkout-1.ejs"), {
-          title: "Checkout",
-          address: address,
-          zipCode: zipCode,
-          country: country,
-          phoneNumber: phoneNumber,
-          message: error,
-          user: req.user,
-          cart: cart,
-          csrfToken: req.csrfToken()
-        }
-      );
+    if (!validated) {
+      let error = new Error("Invalid input fields");
+      res.render(path.resolve(VIEWS, "public", "product", "checkout-1.ejs"), {
+        title: "Checkout",
+        address: address,
+        zipCode: zipCode,
+        country: country,
+        phoneNumber: phoneNumber,
+        message: error,
+        user: req.user,
+        cart: cart,
+        csrfToken: req.csrfToken(),
+      });
     } else {
-      const promise = User.addShippingDetails(req.user, [address, zipCode, country, phoneNumber]);
+      const promise = User.addShippingDetails(req.user, [
+        address,
+        zipCode,
+        country,
+        phoneNumber,
+      ]);
       promise
-        .then(result => {
-          req.flash('success_msg', result)
-          res.redirect('/checkout/2')
+        .then((result) => {
+          req.flash("success_msg", result);
+          res.redirect("/checkout/2");
         })
-        .catch(error => {
+        .catch((error) => {
           res.render(
-            path.resolve(VIEWS, "public", "product", "checkout-1.ejs"), {
+            path.resolve(VIEWS, "public", "product", "checkout-1.ejs"),
+            {
               title: "Checkout",
               address: address,
               zipCode: zipCode,
@@ -252,112 +252,113 @@ export class PublicController {
               user: req.user,
               cart: cart,
               message: error,
-              csrfToken: req.csrfToken()
-            }
+              csrfToken: req.csrfToken(),
+            },
           );
-        })
+        });
     }
   }
 
-  async validatePayment(req, res){
-    let {
-      ccNumber,
-      cvvNumber
-    } = req.body
+  async validatePayment(req, res) {
+    let { ccNumber, cvvNumber } = req.body;
 
     let cart = [];
-    if (typeof(req.user) !== "undefined") {
+    if (typeof req.user !== "undefined") {
       cart = await Product.getUserCart(req.user.id);
     }
 
     // Form validation
     let validated = true;
 
-    let ccNumberCurated = ccNumber.replace(/-/g, "")
-    ccNumberCurated = ccNumberCurated.replace(/ /g, "")
-    let cvvNumberCurated = cvvNumber.replace(/-/g, "")
-    cvvNumberCurated = cvvNumberCurated.replace(/ /g, "")
+    let ccNumberCurated = ccNumber.replace(/-/g, "");
+    ccNumberCurated = ccNumberCurated.replace(/ /g, "");
+    let cvvNumberCurated = cvvNumber.replace(/-/g, "");
+    cvvNumberCurated = cvvNumberCurated.replace(/ /g, "");
 
     let regExVisa = /^4[0-9]{12}(?:[0-9]{3})?$/;
-    let regExMastercard = /^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$/;
+    let regExMastercard =
+      /^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$/;
     let regExCVV = /^([0-9]{3,4})$/;
 
     let error;
-    if (!regExCVV.test(cvvNumberCurated)){
-      validated = false
-      error = new Error("Invalid CVV number")
+    if (!regExCVV.test(cvvNumberCurated)) {
+      validated = false;
+      error = new Error("Invalid CVV number");
     }
-    if(!regExVisa.test(ccNumberCurated) && !regExMastercard.test(ccNumberCurated)){
-      validated = false
-      error = new Error("Invalid Credit Card number")
+    if (
+      !regExVisa.test(ccNumberCurated) &&
+      !regExMastercard.test(ccNumberCurated)
+    ) {
+      validated = false;
+      error = new Error("Invalid Credit Card number");
     }
 
-    if(!validated){
-      res.render(
-        path.resolve(VIEWS, "public", "product", "checkout-2.ejs"), {
-          title: "Checkout",
-          ccNumber: ccNumber,
-          cvvNumber: cvvNumber,
-          message: error,
-          user: req.user,
-          cart: cart,
-          csrfToken: req.csrfToken()
-        }
-      );
-    } else {
+    if (!validated) {
+      res.render(path.resolve(VIEWS, "public", "product", "checkout-2.ejs"), {
+        title: "Checkout",
+        ccNumber: ccNumber,
+        cvvNumber: cvvNumber,
+        message: error,
+        user: req.user,
+        cart: cart,
+        csrfToken: req.csrfToken(),
+      });
+    } else {
       const promise = User.addPaymentDetails(req.user, [ccNumber, cvvNumber]);
       promise
-        .then(result => {
-          req.flash('success_msg', result)
-          res.redirect('/checkout/3')
+        .then((result) => {
+          req.flash("success_msg", result);
+          res.redirect("/checkout/3");
         })
-        .catch(error => {
+        .catch((error) => {
           res.render(
-            path.resolve(VIEWS, "public", "product", "checkout-2.ejs"), {
+            path.resolve(VIEWS, "public", "product", "checkout-2.ejs"),
+            {
               title: "Checkout",
               ccNumber: ccNumber,
               cvvNumber: cvvNumber,
               message: error,
               user: req.user,
               cart: cart,
-              csrfToken: req.csrfToken()
-            }
+              csrfToken: req.csrfToken(),
+            },
           );
-        })
+        });
     }
   }
 
-  async confirmOrder(req, res){
-    const {
-      customer_id,
-      total_order,
-      products_stringified
-    } = req.body
+  async confirmOrder(req, res) {
+    const { customer_id, total_order, products_stringified } = req.body;
 
     const products = JSON.parse(products_stringified);
 
     let promises = [];
-    let createOrderPromise = Product.createOrder(customer_id, total_order)
+    let createOrderPromise = Product.createOrder(customer_id, total_order);
     createOrderPromise
-      .then(orderId => {
-        products.forEach(product => {
-          let promise = Product.createOrderItem(orderId, product.id, product.price, product.quantity)
-          promises.push(promise)
-        })
+      .then((orderId) => {
+        products.forEach((product) => {
+          let promise = Product.createOrderItem(
+            orderId,
+            product.id,
+            product.price,
+            product.quantity,
+          );
+          promises.push(promise);
+        });
       })
-      .catch(e => {
-        res.send(e.message)
-      })
+      .catch((e) => {
+        res.send(e.message);
+      });
 
     Promise.all(promises)
       .then(() => {
-        return Product.deleteUserCart(customer_id)
+        return Product.deleteUserCart(customer_id);
       })
-      .then(result => {
-        res.send(result)
+      .then((result) => {
+        res.send(result);
       })
-      .catch(error => {
-        res.send(error.message)
-      })
+      .catch((error) => {
+        res.send(error.message);
+      });
   }
 }
