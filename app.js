661a13ee01ce96e3d8ff86be5f392ee3caa5ed56
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-03-12 02:47:58 +0700

    Update app node

--- app.js
+++ app.js
@@ -20,107 +20,103 @@ let sessionStore = new MySQLStore(options);
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({});
+// Helmet middleware
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  }),
+);
+
+// Passport config
+import defaultExport from "./src/config/passport.js";
+defaultExport(passport);
+
+// Allow "public" folder to serve static files
+app.use(express.static("public"));
+// Allow Bootstrap
+app.use(
+  "/styles/css",
+  express.static(
+    path.resolve(process.cwd(), "node_modules/bootstrap/dist/css"),
+  ),
+);
+app.use(
+  "/js",
+  express.static(path.resolve(process.cwd(), "node_modules/bootstrap/dist/js")),
+);
+// Allow jQuery
+app.use(
+  "/js",
+  express.static(path.resolve(process.cwd(), "node_modules/jquery/dist")),
+);
+
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+
+// Express session
+app.use(
+  session({
+    key: cookie.name,
+    secret: cookie.secret,
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.warning_msg = req.flash("warning_msg");
+  res.locals.error = req.flash("error");
+  next();
 });
 
-// Helmet middleware
-// app.use(
-//   helmet({
-//     contentSecurityPolicy: false,
-//   }),
-// );
+// Cookie parser
+app.use(cookieParser());
 
-// // Passport config
-// import defaultExport from "./src/config/passport.js";
-// defaultExport(passport);
+// EJS
 
-// // Allow "public" folder to serve static files
-// app.use(express.static("public"));
-// // Allow Bootstrap
-// app.use(
-//   "/styles/css",
-//   express.static(
-//     path.resolve(process.cwd(), "node_modules/bootstrap/dist/css"),
-//   ),
-// );
-// app.use(
-//   "/js",
-//   express.static(path.resolve(process.cwd(), "node_modules/bootstrap/dist/js")),
-// );
-// // Allow jQuery
-// app.use(
-//   "/js",
-//   express.static(path.resolve(process.cwd(), "node_modules/jquery/dist")),
-// );
+// Set path to folder 'views'
+app.set("views", VIEWS);
 
-// // Body parser
-// app.use(express.urlencoded({ extended: false }));
+// PUBLIC-FACING VIEWS
 
-// // Express session
-// app.use(
-//   session({
-//     key: cookie.name,
-//     secret: cookie.secret,
-//     store: sessionStore,
-//     resave: false,
-//     saveUninitialized: false,
-//   }),
-// );
+// Set templating engine
+app.use(expressLayouts);
+app.set("view engine", "ejs");
 
-// // Passport middleware
-// app.use(passport.initialize());
-// app.use(passport.session());
+// Set main layout file
+app.set("layout", path.resolve(VIEWS, "public", "layouts", "layout-main.ejs"));
 
-// // Connect flash
-// app.use(flash());
+// Routes for views in 'views/public'
+import { router as router_public } from "./src/routes/public.js";
+app.use("/", router_public);
 
-// // Global variables
-// app.use((req, res, next) => {
-//   res.locals.success_msg = req.flash("success_msg");
-//   res.locals.error_msg = req.flash("error_msg");
-//   res.locals.warning_msg = req.flash("warning_msg");
-//   res.locals.error = req.flash("error");
-//   next();
-// });
+import { router as router_user } from "./src/routes/user.js";
+app.use("/user", router_user);
 
-// // Cookie parser
-// app.use(cookieParser());
+// DASHBOARD VIEWS
 
-// // EJS
+// Routes for views in 'views/dashboard'
+import { router as router_dashboard } from "./src/routes/dashboard.js";
+app.use("/dashboard", router_dashboard);
 
-// // Set path to folder 'views'
-// app.set("views", VIEWS);
-
-// // PUBLIC-FACING VIEWS
-
-// // Set templating engine
-// app.use(expressLayouts);
-// app.set("view engine", "ejs");
-
-// // Set main layout file
-// app.set("layout", path.resolve(VIEWS, "public", "layouts", "layout-main.ejs"));
-
-// // Routes for views in 'views/public'
-// import { router as router_public } from "./src/routes/public.js";
-// app.use("/", router_public);
-
-// import { router as router_user } from "./src/routes/user.js";
-// app.use("/user", router_user);
-
-// // DASHBOARD VIEWS
-
-// // Routes for views in 'views/dashboard'
-// import { router as router_dashboard } from "./src/routes/dashboard.js";
-// app.use("/dashboard", router_dashboard);
-
-// // 404 Error page
-// app.use((req, res) => {
-//   res.status(404).render(path.resolve(VIEWS, "404.ejs"), {
-//     title: "Error",
-//     layout: "./public/layouts/layout-user",
-//   });
-// });
+// 404 Error page
+app.use((req, res) => {
+  res.status(404).render(path.resolve(VIEWS, "404.ejs"), {
+    title: "Error",
+    layout: "./public/layouts/layout-user",
+  });
+});
 
 app.listen(APP_PORT, () => {
   console.log(`Server started on port ${APP_PORT}...`);
